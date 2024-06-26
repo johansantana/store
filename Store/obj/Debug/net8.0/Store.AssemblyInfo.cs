4b57e27dc88efbd1b6acb7f4d93e2b6151411978
Johan Santana <johancarlosst@gmail.com>
2024-06-26 17:05:52 -0400

    chore: modify ignores

--- Store/obj/Debug/net8.0/Store.AssemblyInfo.cs
+++ Store/obj/Debug/net8.0/Store.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Store")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50e8e944b274a0b850b23db820a69e788e68394d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Store")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Store")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
