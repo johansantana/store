80ffd46c7d07cad336252d4948567efd5f058d73
Johan Santana <johancarlosst@gmail.com>
2024-07-01 22:00:23 -0400

    feat: CRUD Proveedores

---
+++ Store/Pages/Vendor/Edit.cshtml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Store.Data;
+using Store.Model;
+
+namespace Store.Pages.Vendor
+{
+    public class EditModel : PageModel
+    {
+        private readonly Store.Data.StoreContext _context;
+
+        public EditModel(Store.Data.StoreContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Model.Vendor Vendor { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var vendor =  await _context.Vendor.FirstOrDefaultAsync(m => m.Id == id);
+            if (vendor == null)
+            {
+                return NotFound();
+            }
+            Vendor = vendor;
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Vendor).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!VendorExists(Vendor.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool VendorExists(int id)
+        {
+            return _context.Vendor.Any(e => e.Id == id);
+        }
+    }
+}
